3a95c74844fb9b1ece1811c383a6eb2aa7f0ad80
Leon Lau <leondotlau@gmail.com>
2019-08-04 09:59:07 +1000

    height bug

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,9 @@
 @import "@/assets/css/theme.scss";
 html,
 body {
-  height: 100%;
   // background: #211f47;
   background: #eee;
   color: #141414;
-  overflow: auto;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
